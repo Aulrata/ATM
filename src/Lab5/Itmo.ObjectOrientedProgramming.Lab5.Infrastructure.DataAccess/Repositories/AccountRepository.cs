ab202f65735bfff5b2af623c74b23567756e551d
Aulrata <Aulrata@gamil.com>
2023-12-21 02:55:32 +0300

    feat: connectionProvider added

--- src/Lab5/Itmo.ObjectOrientedProgramming.Lab5.Infrastructure.DataAccess/Repositories/AccountRepository.cs
+++ src/Lab5/Itmo.ObjectOrientedProgramming.Lab5.Infrastructure.DataAccess/Repositories/AccountRepository.cs
@@ -1,10 +1,18 @@
-﻿using Itmo.ObjectOrientedProgramming.Lab5.Application.Abstractions.Repositories;
+﻿using Itmo.Dev.Platform.Postgres.Connection;
+using Itmo.ObjectOrientedProgramming.Lab5.Application.Abstractions.Repositories;
 using Itmo.ObjectOrientedProgramming.Lab5.Application.Domain.Accounts;
 
 namespace Itmo.ObjectOrientedProgramming.Lab5.Infrastructure.DataAccess.Repositories;
 
 public class AccountRepository : IAccountRepository
 {
+    private readonly IPostgresConnectionProvider _connectionProvider;
+
+    public AccountRepository(IPostgresConnectionProvider connectionProvider)
+    {
+        _connectionProvider = connectionProvider;
+    }
+
     public void Create(Account account)
     {
         throw new NotImplementedException();
@@ -15,7 +23,7 @@ public class AccountRepository : IAccountRepository
         throw new NotImplementedException();
     }
 
-    public Account GetAccountById(long accountId)
+    public Account? GetAccountById(long accountId)
     {
         throw new NotImplementedException();
     }
--- src/Lab5/Itmo.ObjectOrientedProgramming.Lab5.Infrastructure.DataAccess/Repositories/UserRepository.cs
+++ src/Lab5/Itmo.ObjectOrientedProgramming.Lab5.Infrastructure.DataAccess/Repositories/UserRepository.cs
@@ -1,16 +1,24 @@
-﻿using Itmo.ObjectOrientedProgramming.Lab5.Application.Abstractions.Repositories;
+﻿using Itmo.Dev.Platform.Postgres.Connection;
+using Itmo.ObjectOrientedProgramming.Lab5.Application.Abstractions.Repositories;
 using Itmo.ObjectOrientedProgramming.Lab5.Application.Domain.Users;
 
 namespace Itmo.ObjectOrientedProgramming.Lab5.Infrastructure.DataAccess.Repositories;
 
 public class UserRepository : IUserRepository
 {
+    private readonly IPostgresConnectionProvider _connectionProvider;
+
+    public UserRepository(IPostgresConnectionProvider connectionProvider)
+    {
+        _connectionProvider = connectionProvider;
+    }
+
     public void Create(User user)
     {
         throw new NotImplementedException();
     }
 
-    public User FindByLogin(string login)
+    public User? GetByLogin(string login)
     {
         throw new NotImplementedException();
     }
