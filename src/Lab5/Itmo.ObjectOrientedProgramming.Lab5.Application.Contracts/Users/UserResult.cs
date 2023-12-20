00770abbb65101e3eba5d80c1601a8decd60a706
Aulrata <Aulrata@gamil.com>
2023-12-21 02:51:53 +0300

    feat: user result added

--- src/Lab5/Itmo.ObjectOrientedProgramming.Lab5.Application.Contracts/Users/UserResult.cs
+++ src/Lab5/Itmo.ObjectOrientedProgramming.Lab5.Application.Contracts/Users/UserResult.cs
@@ -1,8 +1,10 @@
 ﻿namespace Itmo.ObjectOrientedProgramming.Lab5.Application.Contracts.Users;
 
-public enum LoginResult
+public enum UserResult
 {
     Success,
     NotFound,
     WrongPassword,
+    NotEnoughMoney,
+    AlreadyExist,
 }
