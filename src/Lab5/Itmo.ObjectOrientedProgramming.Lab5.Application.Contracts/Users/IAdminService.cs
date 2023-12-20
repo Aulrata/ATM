c6b8953333e79cf3c0d0ec681e9fd8a8a64bddc5
Aulrata <Aulrata@gamil.com>
2023-12-21 01:16:24 +0300

    feat: Admin and customer interfaces added

---
+++ src/Lab5/Itmo.ObjectOrientedProgramming.Lab5.Application.Contracts/Users/IAdminService.cs
@@ -0,0 +1,10 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab5.Application.Domain.Operations;
+
+namespace Itmo.ObjectOrientedProgramming.Lab5.Application.Contracts.Users;
+
+public interface IAdminService
+{
+    LoginResult Login(string password);
+    void CreateCustomer(string name, string lastName, string login, string password);
+    IEnumerable<Transaction> ShowAllTransactionsByUser(long userId);
+}
---
+++ src/Lab5/Itmo.ObjectOrientedProgramming.Lab5.Application.Contracts/Users/ICustomerService.cs
@@ -0,0 +1,14 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab5.Application.Domain.Accounts;
+using Itmo.ObjectOrientedProgramming.Lab5.Application.Domain.Operations;
+
+namespace Itmo.ObjectOrientedProgramming.Lab5.Application.Contracts.Users;
+
+public interface ICustomerService
+{
+    LoginResult Login(string login, string password);
+    void CreateAccount();
+    void PutMoneyIntoAccount(long accountId);
+    void WithdrawMoneyFromAccount(long accountId);
+    IEnumerable<Transaction> ShowTransactionsHistory();
+    IEnumerable<Account> ShowAllAccount();
+}
