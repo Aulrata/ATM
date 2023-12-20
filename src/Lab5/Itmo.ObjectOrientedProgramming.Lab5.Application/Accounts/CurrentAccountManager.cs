6b451452fe92223a10e77162f46eb08e362a00c3
Aulrata <Aulrata@gamil.com>
2023-12-21 02:51:39 +0300

    feat: current user and account added

---
+++ src/Lab5/Itmo.ObjectOrientedProgramming.Lab5.Application/Accounts/CurrentAccountManager.cs
@@ -0,0 +1,13 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab5.Application.Domain.Accounts;
+
+namespace Itmo.ObjectOrientedProgramming.Lab5.Application.Accounts;
+
+public class CurrentAccountManager
+{
+    public CurrentAccountManager(Account account)
+    {
+        Account = account;
+    }
+
+    public Account Account { get; set; }
+}
---
+++ src/Lab5/Itmo.ObjectOrientedProgramming.Lab5.Application/Users/CurrentUserManager.cs
@@ -0,0 +1,13 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab5.Application.Domain.Users;
+
+namespace Itmo.ObjectOrientedProgramming.Lab5.Application.Users;
+
+public class CurrentUserManager
+{
+    public CurrentUserManager(User user)
+    {
+        User = user;
+    }
+
+    public User User { get; set; }
+}
